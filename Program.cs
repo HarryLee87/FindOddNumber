c18d0f0230d8e84146b2d1131428d29306a6cc06
HarryLee87 <haewon.harry.lee@gmail.com>
2023-05-28 00:05:54 -0600

    Uploaded finding odd numbers as input

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace OddNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a max number to display all of odd number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= num;  i+=2) 
+            {
+                Console.WriteLine(i);    
+            }
+            Console.WriteLine("************Another Way************");
+            for(int j = 1; j <= num; j++) 
+            {
+                if(j%2 != 0)
+                {
+                    Console.WriteLine($"{j} ");
+                }
+            }
+            string output = "";
+            for (int i = 1; i <= num; i += 2)
+            {
+                output += i + " ";
+            }
+            Console.WriteLine(output);
+            
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (int j = 1; j <= num; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    sb.Append(j + " ");
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+
+        }
+        
+    }
+}
